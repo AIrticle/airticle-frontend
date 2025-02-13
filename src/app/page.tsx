92f5a192fe8d12a95cf942c95ad7b21750ab8af9
SPD3 <sean@ayerdoyles.org>
2025-02-13 14:22:57 -0500

    Getting landing page up and running

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,67 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [showPreview, setShowPreview] = useState(false)
+  const [numOfWords, setNumOfWords] = useState("50")
+
+  function handleChangeNumOfWords(event : React.ChangeEvent<HTMLInputElement>){
+    const input = event.target.value;
+    const numericInput = input.replace(/[^0-9]/g, ''); // Remove non-numeric characters
+    setNumOfWords(numericInput);
+  }
+
+  const article_preview = ("A").repeat(33 * Number(numOfWords));
+
+  return (
+    <div id="full_page" className="p-[0] m-[0] width-full m-[10px] flex-col">
+      <div id="heading" className="border-black border-[2px] rounded-md w-[750px] max-w-[95%] my-[10px] mx-auto">
+        <h1 className="text-center text-5xl">Welcome to AIrticle</h1>
+      </div>
+      <div id="working_area" className="max-w-[95%] w-[750px] my-[10px] mx-auto flex-col">
+        <label htmlFor="notes_input" className="block text-xl my-[5px]">Write your notes here: </label>
+        <textarea 
+          id="notes_input" 
+          className="block w-[100%] rounded-md border border-black border-[2px] focus:ring-blue-500 focus:border-blue-300 resize-none"
+          placeholder="Write your notes here..."
+          rows={20}
+        >
+        </textarea>
+        <div id="buttons"className="flex justify-around">
+          <button 
+            className="mx-[5px] my-[3px] border-black border-[2px] w-[100%] rounded-md border"
+            onClick={() => setShowPreview(true)}
+          >Preview Article</button>
+          <button className="mx-[5px] my-[3px] border-black border-[2px] w-[100%] rounded-md border">Generate Article</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={`rounded-md w-[50vw] h-[85vh] border-black border-[3px] fixed top-[10vh] left-1/2 transform -translate-x-1/2 ${showPreview ? "visible" : "invisible"} bg-white z-2 flex-col space-y-[1%]`} >
+          <div className="he-[1%]"></div>
+          <div className="flex justify-between h-[6%] content-center w-[90%] mx-[5%]">
+            <div className="flex justify-left rounded-md w-[50%]">
+              <label htmlFor="num_of_words" className="my-auto mr-[5%] whitespace-nowrap">Number of Words: </label>
+              <input 
+                id="num_of_words" 
+                type="Number" 
+                className="w-[70%] max-w-[10vw]"
+                onChange={handleChangeNumOfWords}
+                value={numOfWords}
+                min={0}
+                max={1000}
+              ></input>
+            </div>
+            <button 
+              onClick={() => setShowPreview(false)}
+              className="rounded-md border border-black border-[1px] px-[5px]"
+            >Exit</button>
+          </div>
+          <article className="h-[83%] w-[90%] mx-[5%] border border-black border-[1px] rounded-md break-words overflow-y-auto">
+            <p>Here is a preview article: {article_preview}</p>
+          </article>
+          <button className="rounded-md border-black border-[2px] w-[90%] mx-[5%] h-[5%]">Publish</button>
+        </div>
+      </div>
     </div>
   );
 }
