16abfae8473c924de628b351d1bf9cd1a6ffa413
SPD3 <sean@ayerdoyles.org>
2025-02-13 15:34:05 -0500

    A lot more cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,54 +13,55 @@ export default function Home() {
     setNumOfWords(numericInput);
   }
 
-  const article_preview = ("A").repeat(33 * Number(numOfWords));
+  const article_preview = ("AAAA ").repeat(Number(numOfWords));
 
   return (
-    <div id="full_page" className="p-[0] m-[0] width-full m-[10px] flex-col">
-      <div id="heading" className="border-black border-[2px] rounded-md w-[750px] max-w-[95%] my-[10px] mx-auto">
+    <div id="full_page" className="p-[0] m-[0] width-full flex-col height-full space-y-[2vh]">
+      <div id="heading" className="border-black border-[2px] rounded-md w-[750px] max-w-[95%] mx-auto mt-[2%] py-[0.5%]">
         <h1 className="text-center text-5xl">Welcome to AIrticle</h1>
       </div>
-      <div id="working_area" className="max-w-[95%] w-[750px] my-[10px] mx-auto flex-col">
-        <label htmlFor="notes_input" className="block text-xl my-[5px]">Write your notes here: </label>
-        <textarea 
-          id="notes_input" 
-          className="block w-[100%] rounded-md border border-black border-[2px] focus:ring-blue-500 focus:border-blue-300 resize-none"
-          placeholder="Write your notes here..."
-          rows={20}
-        >
-        </textarea>
-        <div id="buttons"className="flex justify-around">
-          <button 
-            className="mx-[5px] my-[3px] border-black border-[2px] w-[100%] rounded-md border"
-            onClick={() => setShowPreview(true)}
-          >Preview Article</button>
-          <button className="mx-[5px] my-[3px] border-black border-[2px] w-[100%] rounded-md border">Generate Article</button>
+      <div id="working_area" className="flex-col w-[750px] max-w-[95%] mx-auto h-[60vh] sm:h-[70vh]">
+        <label htmlFor="notes_input" className="block text-xl h-auto">Write your notes here: </label>
+        <div className="block w-[100%] h-[91%] focus:outline-none resize-none border border-black border-[2px] rounded-md">
+          <textarea 
+            id="notes_input" 
+            className="w-[96%] m-[2%] h-[96%] focus:outline-none resize-none"
+            placeholder="Write your notes here..."
+            rows={20}
+          >
+          </textarea>
         </div>
-        <div className={`rounded-md w-[50vw] h-[85vh] border-black border-[3px] fixed top-[10vh] left-1/2 transform -translate-x-1/2 ${showPreview ? "visible" : "invisible"} bg-white z-2 flex-col space-y-[1%]`} >
-          <div className="he-[1%]"></div>
-          <div className="flex justify-between h-[6%] content-center w-[90%] mx-[5%]">
-            <div className="flex justify-left rounded-md w-[50%]">
-              <label htmlFor="num_of_words" className="my-auto mr-[5%] whitespace-nowrap">Number of Words: </label>
-              <input 
-                id="num_of_words" 
-                type="Number" 
-                className="w-[70%] max-w-[10vw]"
-                onChange={handleChangeNumOfWords}
-                value={numOfWords}
-                min={0}
-                max={1000}
-              ></input>
-            </div>
-            <button 
-              onClick={() => setShowPreview(false)}
-              className="rounded-md border border-black border-[1px] px-[5px]"
-            >Exit</button>
+      </div>
+      <div id="buttons"className="flex justify-around w-[750px] max-w-[95%] mx-auto h-[10vh]">
+        <button 
+          className="mr-[5px] border-black border-[2px] w-[100%] rounded-md border"
+          onClick={() => setShowPreview(true)}
+        ><span className="text-xl">Preview Article</span></button>
+        <button className="ml-[5px] border-black border-[2px] w-[100%] rounded-md border"><span className="text-xl">Analyze Notes</span></button>
+      </div>
+      <div className={`rounded-md w-[750px] max-w-[95%] mx-auto h-[84vh] border-black border-[3px] fixed top-[12vh] left-1/2 transform -translate-x-1/2 ${showPreview ? "visible" : "invisible"} bg-white z-2 flex-col space-y-[1%]`} >
+        <div className="he-[1%]"></div>
+        <div className="flex justify-between h-[6%] content-center w-[90%] mx-[5%]">
+          <div className="flex justify-left rounded-md w-[85%]">
+            <label htmlFor="num_of_words" className="my-auto mr-[5%] whitespace-nowrap">Number of Words: </label>
+            <input 
+              id="num_of_words" 
+              type="number" 
+              onChange={handleChangeNumOfWords}
+              value={numOfWords}
+              min={"0"}
+              max={"1000"}
+            ></input>
           </div>
-          <article className="h-[83%] w-[90%] mx-[5%] border border-black border-[1px] rounded-md break-words overflow-y-auto">
-            <p>Here is a preview article: {article_preview}</p>
-          </article>
-          <button className="rounded-md border-black border-[2px] w-[90%] mx-[5%] h-[5%]">Publish</button>
+          <button 
+            onClick={() => setShowPreview(false)}
+            className="rounded-md border border-black border-[1px] px-[1%] w-[15%]"
+          >Exit</button>
         </div>
+        <article className="h-[80%] w-[90%] mx-[5%] border border-black border-[1px] rounded-md break-words overflow-y-auto">
+          <p>Here is a preview article: {article_preview}</p>
+        </article>
+        <button className="rounded-md border-black border-[2px] w-[90%] mx-[5%] h-[8%]"><span className="text-xl">Publish</span></button>
       </div>
     </div>
   );
