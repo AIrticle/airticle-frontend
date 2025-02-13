e624b1eb27f05aedd749eacdf2b129adc37de535
SPD3 <sean@ayerdoyles.org>
2025-02-13 10:03:39 -0500

    Working on landing page. Not sure how much I'm liking using material UI, so I'm considering switching to tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,45 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import { Box, Button, Stack, TextField } from '@mui/material';
 
 export default function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
+
+  return (
+    <Stack
+      margin="10vh auto"
+      width="90vw"
+      height="80vh"
+      sx={{ border: 1 }}
+      spacing={1}
+    >
+      <Stack
+        width="50%"
+      >
+        <TextField 
+          id="outlined-notes_input" 
+          label="Enter your notes for an article here!" 
+          multiline
+          fullWidth
+          rows={22}
+          />
+        <Stack
+          direction="row"
+          >
+          <Box
+            width={"50%"}
+            paddingLeft="4px"
+            paddingRight="2px"
+            >
+            <Button variant="contained" fullWidth>Publish</Button>
+          </Box>
+          <Box
+            width={"50%"}
+            paddingLeft="2px"
+            paddingRight="4px"
+            >
+            <Button variant="contained" fullWidth>Parse Notes</Button>
+          </Box>
+        </Stack>
+      </Stack>
+    </Stack>
+  )
 }
